a2f7ebad7b9e9097d0263a082bf1d78eb7931620
Alexander Karlsson <alexander@kodamera.se>
2024-06-19 22:56:38 +0200

    Rewrite to make code easier to interpret

--- pdns-sinkhole-generator.py
+++ pdns-sinkhole-generator.py
@@ -1,51 +1,49 @@
 import sys
 import requests
-import toml
+import yaml
 from pathlib import Path
 from typing import List
 
-def luaify(input: List[str]) -> str:
-    input.sort()
-    input = list(dict.fromkeys(input))  # Deduplicate
-    output = "return{\n"
-    for entry in input:
-        output += f"    \"{entry}\",\n"
-    output = output.rstrip(',\n') + "\n}"
-    return output
+def convert_to_lua_table(url_list: List[str]) -> str:
+    url_list.sort()
+    url_list = list(dict.fromkeys(url_list))  # Deduplicate
+    lua_table = "return{\n"
+    for url in url_list:
+        lua_table += f"    \"{url}\",\n"
+    lua_table = lua_table.rstrip(',\n') + "\n}"
+    return lua_table
 
-def process(tgt: List[str], path: Path) -> None:
-    urls = []
-    for url in tgt:
+def process_urls(urls_to_process: List[str], output_file_path: Path) -> None:
+    cleaned_urls = []
+    for url in urls_to_process:
         try:
             response = requests.get(url)
             response.raise_for_status()
-            body = response.text
+            response_body = response.text
         except requests.RequestException as e:
-            print(f"Got busted URL: {url} with error {e}")
+            print(f"Failed to fetch URL: {url} with error {e}")
             continue
 
-        for line in body.splitlines():
+        for line in response_body.splitlines():
             if not line or line.startswith('#'):
                 continue
             line = line.split('#')[0].strip()
-            urls.append(line.strip())
+            cleaned_urls.append(line.strip())
 
-    output = luaify(urls)
-    with path.open('w') as o:
-        o.write(output)
+    lua_formatted_output = convert_to_lua_table(cleaned_urls)
+    with output_file_path.open('w') as output_file:
+        output_file.write(lua_formatted_output)
 
-def main(config_path: Path) -> None:
-    with config_path.open('r') as cf:
-        cfg = cf.read()
-    
-    config = toml.loads(cfg)
+def main(configuration_file_path: Path) -> None:
+    with configuration_file_path.open('r') as config_file:
+        configuration = yaml.safe_load(config_file)
 
-    process(config['blocklists'], Path(config['blocklist_output']))
-    process(config['permitted'], Path(config['permitted_output']))
+    process_urls(configuration['blocklists'], Path(configuration['blocklist_output']))
+    process_urls(configuration['permitted'], Path(configuration['permitted_output']))
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: python pdns-sinkhole-generator.py <config_path>")
+        print("Usage: python script.py <config_path>")
         sys.exit(1)
     
     config_path = Path(sys.argv[1])
